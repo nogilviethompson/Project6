269444b56ced22461c5edf70b584e59ae614fcfb
Nicky Ogilvie-Thompson <nogilviethompson@gmail.com>
2017-05-03 13:17:00 -0400

    Current Stuff - Trimmed down but still takes too much memory

--- commandLineTest.cpp
+++ commandLineTest.cpp
@@ -31,12 +31,10 @@ struct pairs{
 	  unsigned short pathPointer;
 	  int position;
  };
-map <string, vector <struct pairs> > words;
-vector<string> filePaths;
 
-void ProcessDirectory(string directory);
-void ProcessFile(string file, vector<string>& filePaths);
-void ProcessEntity(struct dirent* entity);
+void ProcessDirectory(string directory, vector<string>& filePaths, map<string, vector<struct pairs> >& words);
+void ProcessFile(string file, vector<string>& filePaths, map<string, vector<struct pairs> >& words);
+void ProcessEntity(struct dirent* entity, vector<string>& filePaths, map<string, vector<struct pairs> >& words);
 bool hasEnding (string const &fullString, string const &ending);
 void build(string file, unsigned int point, map<string, vector<struct pairs> >& words);
 vector<struct pairs> search(string word, map<string, vector<struct pairs> > words);
@@ -51,11 +49,14 @@ int main()
   string directory = "";
   string display;
   ifstream infile;
-  ProcessDirectory(directory);
+  map <string, vector <struct pairs> > words;
+  vector<string> filePaths;
+  
+  ProcessDirectory(directory, filePaths, words);
   
   cout << "Please enter a word to search for: ";
   cin >> word;
-  transform(word.begin(), word.end(), word.begin(), ::toupper);
+  transform(word.begin(), word.end(), word.begin(), ::tolower);
   stemmer(word);
   refs = search(word, words);
   
@@ -90,8 +91,7 @@ bool hasEnding (string const &fullString, string const &ending) {
   }
 }
 
-void ProcessDirectory(string directory)
-{
+void ProcessDirectory(string directory, vector<string>& filePaths, map<string, vector<struct pairs> >& words){
   string dirToOpen = path + directory;
   DIR *dir;
   dir = opendir(dirToOpen.c_str());
@@ -111,7 +111,7 @@ void ProcessDirectory(string directory)
 
   while(entity != NULL)
     {
-      ProcessEntity(entity);
+      ProcessEntity(entity, filePaths, words);
       entity = readdir(dir);
     }
 
@@ -120,7 +120,7 @@ void ProcessDirectory(string directory)
   closedir(dir);
 }
 
-void ProcessEntity(struct dirent* entity)
+void ProcessEntity(struct dirent* entity, vector<string>& filePaths, map<string, vector<struct pairs> >& words)
 {
   //find entity type
   if(entity->d_type == DT_DIR)
@@ -132,13 +132,13 @@ void ProcessEntity(struct dirent* entity)
 	}
 
       //it's an directory so process it
-      ProcessDirectory(string(entity->d_name));
+      ProcessDirectory(string(entity->d_name), filePaths, words);
       return;
     }
 
   if(entity->d_type == DT_REG)
     {//regular file
-      ProcessFile(string(entity->d_name), filePaths);
+      ProcessFile(string(entity->d_name), filePaths, words);
       return;
     }
 
@@ -147,7 +147,7 @@ void ProcessEntity(struct dirent* entity)
   cout << "Not a file or directory: " << entity->d_name << endl;
 }
 
-void ProcessFile(string file, vector<string>& filePaths)
+void ProcessFile(string file, vector<string>& filePaths, map<string, vector<struct pairs> >& words)
 {
   string fileType = ".txt";
   if (hasEnding(file,fileType)) {
@@ -177,8 +177,8 @@ void build(string fullPath, unsigned int point, map<string, vector<struct pairs>
             {
 				word = line.substr(found2, found-found2);
 				stemmer(word);
-				transform(word.begin(), word.end(), word.begin(), ::toupper);
-				if (!commonWord(word)){
+				transform(word.begin(), word.end(), word.begin(), ::tolower);
+				if (commonWord(word) == false){
 					locations.pathPointer = pathPosition;
 					locations.position = pos;
 					words[word].push_back(locations);
@@ -211,111 +211,541 @@ vector<struct pairs> search(string word, map<string, vector<struct pairs> > word
 
 bool commonWord(string word){
 	bool common = false;
-	if (word == "A"){
+	if (word == "a"){
 		common = true;
-	}else if (word == "THE"){
+	}
+	else if (word == "about"){
 		common = true;
-	}else if (word == "IS"){
+	}
+	else if (word == "above"){
 		common = true;
-	}else if (word == "AM"){
+	}
+	else if (word == "after"){
 		common = true;
-	}else if (word == "ARE"){
+	}
+	else if (word == "again"){
 		common = true;
-	}else if (word == "HE"){
+	}
+	else if (word == "against"){
 		common = true;
-	}else if (word == "SHE"){
+	}
+	else if (word == "all"){
 		common = true;
-	}else if (word == "I"){
+	}
+	else if (word == "am"){
 		common = true;
-	}else if (word == "IT"){
+	}
+	else if (word == "an"){
 		common = true;
-	}else if (word == "WHO"){
+	}
+	else if (word == "and"){
 		common = true;
-	}else if (word == "AT"){
+	}
+	else if (word == "any"){
 		common = true;
-	}else if (word == "AND"){
+	}
+	else if (word == "are"){
 		common = true;
-	}else if (word == "OR"){
+	}
+	else if (word == "aren't"){
 		common = true;
-	}else if (word == "BUT"){
+	}
+	else if (word == "as"){
 		common = true;
-	}else if (word == "IF"){
+	}
+	else if (word == "at"){
 		common = true;
-	}else if (word == "ELSE"){
+	}
+	else if (word == "be"){
 		common = true;
-	}else if (word == "THEY"){
+	}
+	else if (word == "because"){
 		common = true;
-	}else if (word == "YOU"){
+	}
+	else if (word == "been"){
 		common = true;
-	}else if (word == "WE"){
+	}
+	else if (word == "before"){
 		common = true;
-	}else if (word == "US"){
+	}
+	else if (word == "being"){
 		common = true;
-	}else if (word == "HIM"){
+	}
+	else if (word == "below"){
 		common = true;
-	}else if (word == "HER"){
+	}
+	else if (word == "between"){
 		common = true;
-	}else if (word == "THEM"){
+	}
+	else if (word == "both"){
 		common = true;
-	}else if (word == "THEIR"){
+	}
+	else if (word == "but"){
 		common = true;
-	}else if (word == "THERE"){
+	}
+	else if (word == "by"){
 		common = true;
-	}else if (word == "HERE"){
+	}
+	else if (word == "can't"){
 		common = true;
-	}else if (word == "AS"){
+	}
+	else if (word == "cannot"){
 		common = true;
-	}else if (word == "BY"){
+	}
+	else if (word == "could"){
 		common = true;
-	}else if (word == "ELSE"){
+	}
+	else if (word == "couldn't"){
 		common = true;
-	}else if (word == "FOR"){
+	}
+	else if (word == "did"){
 		common = true;
-	}else if (word == "DO"){
+	}
+	else if (word == "didn't"){
 		common = true;
-	}else if (word == "GO"){
+	}
+	else if (word == "do"){
 		common = true;
-	}else if (word == "HIS"){
+	}
+	else if (word == "does"){
 		common = true;
-	}else if (word == "HERS"){
+	}
+	else if (word == "doesn't"){
 		common = true;
-	}else if (word == "THEIRS"){
+	}
+	else if (word == "doing"){
 		common = true;
-	}else if (word == "ITS"){
+	}
+	else if (word == "don't"){
 		common = true;
-	}else if (word == "IN"){
+	}
+	else if (word == "down"){
 		common = true;
-	}else if (word == "ON"){
+	}
+	else if (word == "during"){
 		common = true;
-	}else if (word == "JUST"){
+	}
+	else if (word == "each"){
 		common = true;
-	}else if (word == "MR"){
+	}
+	else if (word == "few"){
 		common = true;
-	}else if (word == "MS"){
+	}
+	else if (word == "for"){
 		common = true;
-	}else if (word == "MRS"){
+	}
+	else if (word == "from"){
 		common = true;
-	}else if (word == "MISTER"){
+	}
+	else if (word == "further"){
 		common = true;
-	}else if (word == "MY"){
+	}
+	else if (word == "had"){
 		common = true;
-	}else if (word == "YES"){
+	}
+	else if (word == "hadn't"){
 		common = true;
-	}else if (word == "NO"){
+	}
+	else if (word == "has"){
 		common = true;
-	}else if (word == "NOR"){
+	}
+	else if (word == "hasn't"){
 		common = true;
-	}else if (word == "OF"){
+	}
+	else if (word == "have"){
 		common = true;
-	}else if (word == "OFF"){
+	}
+	else if (word == "haven't"){
 		common = true;
-	}else if (word == "OH"){
+	}
+	else if (word == "having"){
 		common = true;
-	}else if (word == "OUR"){
+	}
+	else if (word == "he"){
 		common = true;
-	}else if (word == "PER"){
+	}
+	else if (word == "he'd"){
 		common = true;
-	}else if (word == "SAY"){
+	}
+	else if (word == "he'll"){
+		common = true;
+	}
+	else if (word == "he's"){
+		common = true;
+	}
+	else if (word == "her"){
+		common = true;
+	}
+	else if (word == "here"){
+		common = true;
+	}
+	else if (word == "here's"){
+		common = true;
+	}
+	else if (word == "hers"){
+		common = true;
+	}
+	else if (word == "herself"){
+		common = true;
+	}
+	else if (word == "him"){
+		common = true;
+	}
+	else if (word == "himself"){
+		common = true;
+	}
+	else if (word == "his"){
+		common = true;
+	}
+	else if (word == "how"){
+		common = true;
+	}
+	else if (word == "how's"){
+		common = true;
+	}
+	else if (word == "i"){
+		common = true;
+	}
+	else if (word == "i'd"){
+		common = true;
+	}
+	else if (word == "i'll"){
+		common = true;
+	}
+	else if (word == "i'm"){
+		common = true;
+	}
+	else if (word == "i've"){
+		common = true;
+	}
+	else if (word == "if"){
+		common = true;
+	}
+	else if (word == "in"){
+		common = true;
+	}
+	else if (word == "into"){
+		common = true;
+	}
+	else if (word == "is"){
+		common = true;
+	}
+	else if (word == "isn't"){
+		common = true;
+	}
+	else if (word == "it"){
+		common = true;
+	}
+	else if (word == "it's"){
+		common = true;
+	}
+	else if (word == "its"){
+		common = true;
+	}
+	else if (word == "itself"){
+		common = true;
+	}
+	else if (word == "let's"){
+		common = true;
+	}
+	else if (word == "me"){
+		common = true;
+	}
+	else if (word == "more"){
+		common = true;
+	}
+	else if (word == "most"){
+		common = true;
+	}
+	else if (word == "mr"){
+		common = true;
+	}
+	else if (word == "mrs"){
+		common = true;
+	}
+	else if (word == "ms"){
+		common = true;
+	}
+	else if (word == "must"){
+		common = true;
+	}
+	else if (word == "mustn't"){
+		common = true;
+	}
+	else if (word == "my"){
+		common = true;
+	}
+	else if (word == "myself"){
+		common = true;
+	}
+	else if (word == "no"){
+		common = true;
+	}
+	else if (word == "nor"){
+		common = true;
+	}
+	else if (word == "not"){
+		common = true;
+	}
+	else if (word == "of"){
+		common = true;
+	}
+	else if (word == "off"){
+		common = true;
+	}
+	else if (word == "on"){
+		common = true;
+	}
+	else if (word == "once"){
+		common = true;
+	}
+	else if (word == "only"){
+		common = true;
+	}
+	else if (word == "or"){
+		common = true;
+	}
+	else if (word == "other"){
+		common = true;
+	}
+	else if (word == "ought"){
+		common = true;
+	}
+	else if (word == "our"){
+		common = true;
+	}
+	else if (word == "ours"){
+		common = true;
+	}
+	else if (word == "ourselves"){
+		common = true;
+	}
+	else if (word == "out"){
+		common = true;
+	}
+	else if (word == "over"){
+		common = true;
+	}
+	else if (word == "own"){
+		common = true;
+	}
+	else if (word == "same"){
+		common = true;
+	}
+	else if (word == "shall"){
+		common = true;
+	}
+	else if (word == "shan't"){
+		common = true;
+	}
+	else if (word == "she"){
+		common = true;
+	}
+	else if (word == "she'd"){
+		common = true;
+	}
+	else if (word == "she'll"){
+		common = true;
+	}
+	else if (word == "she's"){
+		common = true;
+	}
+	else if (word == "should"){
+		common = true;
+	}
+	else if (word == "shouldn't"){
+		common = true;
+	}
+	else if (word == "so"){
+		common = true;
+	}
+	else if (word == "some"){
+		common = true;
+	}
+	else if (word == "such"){
+		common = true;
+	}
+	else if (word == "than"){
+		common = true;
+	}
+	else if (word == "that"){
+		common = true;
+	}
+	else if (word == "that's"){
+		common = true;
+	}
+	else if (word == "the"){
+		common = true;
+	}
+	else if (word == "their"){
+		common = true;
+	}
+	else if (word == "theirs"){
+		common = true;
+	}
+	else if (word == "them"){
+		common = true;
+	}
+	else if (word == "themselves"){
+		common = true;
+	}
+	else if (word == "then"){
+		common = true;
+	}
+	else if (word == "there"){
+		common = true;
+	}
+	else if (word == "there's"){
+		common = true;
+	}
+	else if (word == "these"){
+		common = true;
+	}
+	else if (word == "they"){
+		common = true;
+	}
+	else if (word == "they'd"){
+		common = true;
+	}
+	else if (word == "they'll"){
+		common = true;
+	}
+	else if (word == "they're"){
+		common = true;
+	}
+	else if (word == "they've"){
+		common = true;
+	}
+	else if (word == "this"){
+		common = true;
+	}
+	else if (word == "those"){
+		common = true;
+	}
+	else if (word == "through"){
+		common = true;
+	}
+	else if (word == "to"){
+		common = true;
+	}
+	else if (word == "too"){
+		common = true;
+	}
+	else if (word == "under"){
+		common = true;
+	}
+	else if (word == "until"){
+		common = true;
+	}
+	else if (word == "up"){
+		common = true;
+	}
+	else if (word == "very"){
+		common = true;
+	}
+	else if (word == "was"){
+		common = true;
+	}
+	else if (word == "wasn't"){
+		common = true;
+	}
+	else if (word == "we"){
+		common = true;
+	}
+	else if (word == "we'd"){
+		common = true;
+	}
+	else if (word == "we'll"){
+		common = true;
+	}
+	else if (word == "we're"){
+		common = true;
+	}
+	else if (word == "we've"){
+		common = true;
+	}
+	else if (word == "were"){
+		common = true;
+	}
+	else if (word == "weren't"){
+		common = true;
+	}
+	else if (word == "what"){
+		common = true;
+	}
+	else if (word == "what's"){
+		common = true;
+	}
+	else if (word == "when"){
+		common = true;
+	}
+	else if (word == "when's"){
+		common = true;
+	}
+	else if (word == "where"){
+		common = true;
+	}
+	else if (word == "where's"){
+		common = true;
+	}
+	else if (word == "which"){
+		common = true;
+	}
+	else if (word == "while"){
+		common = true;
+	}
+	else if (word == "who"){
+		common = true;
+	}
+	else if (word == "who's"){
+		common = true;
+	}
+	else if (word == "whom"){
+		common = true;
+	}
+	else if (word == "why"){
+		common = true;
+	}
+	else if (word == "why's"){
+		common = true;
+	}
+	else if (word == "with"){
+		common = true;
+	}
+	else if (word == "won't"){
+		common = true;
+	}
+	else if (word == "would"){
+		common = true;
+	}
+	else if (word == "wouldn't"){
+		common = true;
+	}
+	else if (word == "you"){
+		common = true;
+	}
+	else if (word == "you'd"){
+		common = true;
+	}
+	else if (word == "you'll"){
+		common = true;
+	}
+	else if (word == "you're"){
+		common = true;
+	}
+	else if (word == "you've"){
+		common = true;
+	}
+	else if (word == "your"){
+		common = true;
+	}
+	else if (word == "yours"){
+		common = true;
+	}
+	else if (word == "yourself"){
+		common = true;
+	}
+	else if (word == "yourselves"){
 		common = true;
 	}
 	
